0a99fdd8a55998a0421b5952c5f9d121ec1cd5c7
serdar tunç <hero4738@gmail.com>
2025-03-27 21:33:47 +0300

    #

---
+++ CRM.Server/Controllers/EmployeesController.cs
@@ -0,0 +1,93 @@
+﻿using CRM.Server.Data;
+using CRM.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRM.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        //DI
+        private readonly AppDbContext _appDbContext;
+        public EmployeesController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+            //_appDbContext.Employees
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployyes()
+        {
+            return await _appDbContext.Employees.ToListAsync();
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> CreateEmployee(Employee emp)
+        {
+            _appDbContext.Employees.AddAsync(emp);
+            await _appDbContext.SaveChangesAsync();
+            return CreatedAtAction("GetEmployeeById", new { id = emp.Id }, emp);
+        }
+        //https://localhost:7133/api/Employees/1
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmloyeesId(int id)
+        {
+            var emp = await _appDbContext.Employees.FindAsync(id);
+            //var amp = await _appDbContext.Employees.FirstOrDefaultAsync(e=>e.Id == id); 
+            if (emp == null)
+            {
+                return NotFound();
+            }
+            // return Ok(emp);
+            return Ok(emp);
+        }
+        //https://localhost:7133/api/Employees/1
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Employee>> UpdateEmployeeById(int id, Employee emp)
+        {
+            if (id != emp.Id)
+            {
+                BadRequest();
+            }
+            _appDbContext.Entry(emp).State = EntityState.Modified;
+            try
+            {
+                await _appDbContext.SaveChangesAsync();//çalıştırılacak kod bıloğu
+            }
+            catch (Exception ex)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+        private bool EmployeeExists(int id)
+        {
+            return _appDbContext.Employees.Any(e => e.Id == id);
+
+        }
+        //https://localhost:7133/api/Employees/1
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Employee>> DeleteEmployeeById(int id) 
+        {
+            var emp = await _appDbContext.Employees.FindAsync(id);
+            if(emp == null)
+            {
+                return NotFound();
+            }
+            _appDbContext.Employees.Remove(emp);
+            await _appDbContext.SaveChangesAsync();
+            return NoContent(); 
+        
+        }
+    }    
+
+}
