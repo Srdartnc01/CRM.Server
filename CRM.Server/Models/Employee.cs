ca4eb11965eee495b600d82c37b44d3deb02a840
serdar tunç <hero4738@gmail.com>
2025-03-27 18:33:46 +0300

    Add project files.

---
+++ CRM.Server/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace CRM.Server.Models
+{
+    public class Employee
+    {
+
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+
+    }
+}
